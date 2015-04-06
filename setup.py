a2e2f9fff97161d6496363036bd5856ba4d94bba
Jay Luker <lbjay@reallywow.com>
2015-04-06 10:00:35 -0400

    version tweak to comply with standard semver.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='ims_lti_py',
-    version='0.6',
+    version='0.6.0',
     description=('A Python library to help implement IMS '
                  'LTI tool consumers and providers'),
-    author='Anson MacKeracher',
-    author_email='anson@tophatmonocle.com',
-    url='https://github.com/tophatmonocle/ims_lti_py',
+    author='Jay Luker',
+    author_email='jay_luker@harvard.edu',
+    url='https://github.com/harvard-dce/ims_lti_py',
     packages=find_packages(),
     install_requires=['lxml', 'oauth2'],
     license='MIT License',
